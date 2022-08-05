6554b515bc45fc73c71b87175f94b8092cd6e83c
unknown <mapasekasemela1@gmail.com>
2022-08-05 14:19:29 +0200

    added whole project

--- app.js
+++ app.js
@@ -16,23 +16,16 @@ function addExpense(e){
     document.getElementById('balanceS').innerHTML = "R"+total;
 
 
-
     e.preventDefault();
 
     // get type, name, date, and amount
     let type = document.getElementById('type').value;
-    let name = document.getElementById('name').value;
-    let date = document.getElementById('date').value;
     let amount = document.getElementById('amount').value;
 
     if(type != 'chooseOne' 
-        && name.length > 0 
-        && date != 0 
         && amount > 0){
         const expense = {
             type, 
-            name, 
-            date,
             amount, 
             id: expenses.length > 0 ? expenses[expenses.length - 1].id + 1 : 1,
         }
@@ -56,8 +49,6 @@ const showExpenses = () => {
         expenseTable.innerHTML += `
             <tr>
                 <td>${expenses[i].type}</td>
-                <td>${expenses[i].name}</td>
-                <td>${expenses[i].date}</td>
                 <td>R${expenses[i].amount}</td>
                 <td><a class="deleteButton" onclick="deleteExpense(${expenses[i].id})">
                     Bin</td>
